ced2d0631e48329f141cdd9c70cc16d19a7141ed
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-01-15 21:26:25 -0800

    isolated link and headline from innerHTML

--- scraper_bloomberg.py
+++ scraper_bloomberg.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 
 DRIVER_PATH = '/Users/Jasmit/Documents/GitHub/AI_trader/chromedriver-win64/chromedriver.exe'
 
 service = Service(executable_path=DRIVER_PATH)
 options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
 
 #enable headless mode
 #options.headless = True
@@ -19,7 +21,15 @@ articles = driver.find_elements(By.XPATH, "//div[@data-type='article']")
 print("-------------------------")
 print(articles)
 for article in articles:
-    #title = article.find_element(By.XPATH, ".//h2").text
-    content = article.get_attribute('innerHTML')
-    print(f"Content: {content}\n")
-driver.quit()
+	content = article.get_attribute('innerHTML')
+	soup = BeautifulSoup(content, 'html.parser')
+	link = soup.find('a')	
+	href_link = "https://bloomberg.com"+link.get('href')
+	headline_text = soup.select_one('div[data-component="headline"]').get_text(strip=True)
+
+	#headline = article.find_element(By.XPATH, ".//div[@data-component='headline']")
+	#headline_html = headline.get_attribute("outerHTML")
+	#soup_1 = BeautifulSoup(headline_html, 'html.parser')
+	#headline_text = soup.get_text(strip=True)
+	print(f"link: {href_link}\n headline: {headline_text}\n")
+#driver.quit()
