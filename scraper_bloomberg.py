16731d1f7b2d0222371827d2c9e84060707f22aa
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-01-16 15:09:10 -0800

    logging data to folder

--- scraper_bloomberg.py
+++ scraper_bloomberg.py
@@ -16,10 +16,13 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=service, options=options)
 driver.get('https://bloomberg.com')
-#print(driver.page_source)
 articles = driver.find_elements(By.XPATH, "//div[@data-type='article']")
 print("-------------------------")
 print(articles)
+
+link_list = []
+headline_list = []
+
 for article in articles:
 	content = article.get_attribute('innerHTML')
 	soup = BeautifulSoup(content, 'html.parser')
@@ -31,5 +34,13 @@ for article in articles:
 	#headline_html = headline.get_attribute("outerHTML")
 	#soup_1 = BeautifulSoup(headline_html, 'html.parser')
 	#headline_text = soup.get_text(strip=True)
+	link_list.append(href_link)
+	headline_list.append(headline_text)
 	print(f"link: {href_link}\n headline: {headline_text}\n")
 #driver.quit()
+
+output_file_path = "/Users/Jasmit/Documents/GitHub/AI_trader/logs/bloomberg_frontpage.txt"
+
+with open(output_file_path, 'w', encoding='utf-8') as file:
+    for headline, link in zip(headline_list, link_list):
+        file.write(f"{headline}\n{link}\n\n")
